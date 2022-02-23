1c09f4cc741db2e474da5d9223fdd461af6d0e00
gulernurdonmez <gule0153@stud.kea.dk>
2022-02-23 18:16:51 +0100

    first commit

---
+++ src/main/java/com/example/daily_paper/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.daily_paper.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/firstElement")
+    public String firstElement(){
+        return "btCopy";
+    }
+
+
+}
